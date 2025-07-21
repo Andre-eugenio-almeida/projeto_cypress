3c22c81b3cdc1b882d471c54984aa4b8b1364af4
André Almeida <almeida.ae@outlook.com>
2025-07-21 20:53:44 -0300

    Atualizações

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,13 +2,17 @@ describe('login',() => {
     it('realizae login com sucesso',() => {
         
       // arrange
-      
+      cy.visit('https://saucedemo.com/')  
 
 
       // act
-
-
+      cy.get('[data-test="username"]').type('standard_user') 
+      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="login-button"]').click()
 
       // assert 
+      cy.url().should('eq','https://www.saucedemo.com/inventory.html')
+
+
     })
 })
