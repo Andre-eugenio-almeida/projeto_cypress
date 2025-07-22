cf6273978181eb834798bbeffe08ec3819b7263d
André Almeida <almeida.ae@outlook.com>
2025-07-21 21:53:34 -0300

    Atualizações

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,12 +7,34 @@ describe('login',() => {
 
       // act
       cy.get('[data-test="username"]').type('standard_user') 
+
       cy.get('[data-test="password"]').type('secret_sauce')
+
       cy.get('[data-test="login-button"]').click()
 
       // assert 
       cy.url().should('eq','https://www.saucedemo.com/inventory.html')
 
+    })
+      it('realizar login informando credenciais inválidas',() => {
+
+    // arrange
+      cy.visit('https://saucedemo.com/')  
+
+
+    // act
+     cy.get('[data-test="username"]').type('use.invalid') 
+
+      cy.get('[data-test="password"]').type('senha')
+
+      cy.get('[data-test="login-button"]').click()
+
+      
+    // assert 
+      cy.get('[data-test="error"]')
+        .should('contain.text','Username and password do not match any user in this service')
+
+    
 
     })
 })
