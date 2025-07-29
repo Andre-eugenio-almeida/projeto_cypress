604fda11a3535c23d0b7cd40c95aeb4195b35b5d
André Almeida <almeida.ae@outlook.com>
2025-07-29 18:31:38 -0300

    Atualizando teste

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -7,12 +7,17 @@ describe('carrinho', () =>{
 
       cy.get('[data-test="username"]').type('standard_user') 
 
-      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="password"]').type('secret_sauce') 
 
       cy.get('[data-test="login-button"]').click()
       
       //act
       cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+      // assert
+        cy.get('.shopping_cart_badge')
+            .should('be.visible')
+            .and('have.text', '1')
     })
 
 })
